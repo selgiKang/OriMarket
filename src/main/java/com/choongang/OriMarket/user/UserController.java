de18da142c51df4a2dd70d96714a581842c46e03
jaekun01 <jaekun12345@naver.com>
2023-07-13 16:43:32 +0900

    aa

--- src/main/java/com/choongang/OriMarket/user/UserController.java
+++ src/main/java/com/choongang/OriMarket/user/UserController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.model.IModel;
 
 import javax.servlet.http.Cookie;
