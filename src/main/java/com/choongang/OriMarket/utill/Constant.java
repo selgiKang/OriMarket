414ce105684324ab517dc880b05e81dfe3cc2e88
jaekun01 <jaekun12345@naver.com>
2023-07-25 21:01:03 +0900

    123

--- src/main/java/com/choongang/OriMarket/utill/Constant.java
+++ src/main/java/com/choongang/OriMarket/utill/Constant.java
@@ -6,4 +6,6 @@ public class Constant {
 
     public static final String IMAGE_PATH = "src\\main\\resources\\static\\img\\store\\";
 
+    public static final String REVIEW_IMAGE_PATH = "src\\main\\resources\\static\\img\\review\\";
+
 }
