4e884b6a7d246709735187fe301f6eaad00a3164
jaekun01 <jaekun12345@naver.com>
2023-07-27 13:50:14 +0900

    가게 오픈 클로즈

--- src/main/java/com/choongang/OriMarket/business/store/BusinessStore.java
+++ src/main/java/com/choongang/OriMarket/business/store/BusinessStore.java
@@ -49,7 +49,7 @@ public class BusinessStore {
     private String buStoreImageUrl;
 
     @Column
-    private String status;
+    private String status = "CLOSE";
 
     //사업자 번호
     @ManyToOne(fetch = FetchType.LAZY) //lazy가 성능최적화
