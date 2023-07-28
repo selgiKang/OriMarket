e79da5c506c0d5d9119316d886a203ef35c5fff6
jaekun01 <jaekun12345@naver.com>
2023-07-28 14:34:02 +0900

    라이더

--- src/main/java/com/choongang/OriMarket/business/store/BusinessStoreService.java
+++ src/main/java/com/choongang/OriMarket/business/store/BusinessStoreService.java
@@ -36,22 +36,22 @@ public class BusinessStoreService {
     public void save(BusinessStore businessStore, HttpSession session, Model model, String s){
 
         Object buUserNumber = session.getAttribute("buUserNumber");
-        System.out.println("1번");
+
         // 사업자가 로그인할때 생성된 마켓번호로 해당마켓번호의 마켓을 찾는다.
         Long marketSeq = Long.valueOf((session.getAttribute("marketSeq")).toString());
-        System.out.println("2번");
+
         Market bymarket = marketRepository.findById(marketSeq).orElseThrow();
-        System.out.println("3번");
+
 
         // 가게등록을 할때 어느 시장의 가게인지 알아야하기 때문에 상인이속한 시장을 가게에 등록해준다.
         businessStore.setMarket(bymarket);
-        System.out.println("4번");
+
 
         // 로그인할 때 생성된 사업자 번호로 사업자를 찾는다.
         BusinessUser businessUser = businessUserRepository.findById((Long)buUserNumber).orElseThrow();
-        System.out.println("5번");
 
-        System.out.println("6번");
+
+
         // 가게등록할때 누구 가게인지 알아야하기 때문에 찾은 사업자를 가게에 등록해준다.
         businessStore.setBusinessUser(businessUser);
         // 가게에 등록한 사진 정보를 등록해준다.
---
+++ src/main/java/com/choongang/OriMarket/rider/Rider.java
@@ -0,0 +1,43 @@
+package com.choongang.OriMarket.rider;
+
+import com.choongang.OriMarket.review.Review;
+import com.choongang.OriMarket.user.UserAddress;
+import com.choongang.OriMarket.user.UserMarket;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Rider")
+@Getter
+@Setter
+//생성자
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "rider_seq", nullable = false)
+    private Long riderSeq;
+
+    private String riderId;
+
+    private String riderPassword;
+
+    private String riderName;
+
+    private String riderPhone;
+
+    private String riderAddress;
+
+    private String driverLicense;
+
+
+
+
+
+}
